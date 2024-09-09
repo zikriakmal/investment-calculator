f5b5105473eb67ba4639ea7487a772772722795b
Zikri Akmal S <zikriakmale@gmail.com>
2024-09-09 11:59:59 +0700

    feat: migrating to signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentInput } from './investment-input.model';
@@ -12,14 +12,16 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
 })
 export class AppComponent {
 
-  resultsData?: {
+  resultsData = signal<{
     year: number;
     interest: number;
     valueEndOfYear: number;
     annualInvestment: number;
     totalInterest: number;
     totalAmountInvested: number;
-  }[];
+  }[] | undefined>(undefined)
+
+
 
   calculateInvestmentResults(data: InvestmentInput) {
     const annualData = [];
@@ -42,7 +44,7 @@ export class AppComponent {
       });
     }
 
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 
 }
